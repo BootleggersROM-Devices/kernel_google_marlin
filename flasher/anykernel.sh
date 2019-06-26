b6ae77919df4413de15768a057230428e2829ad6
shagbag913 <sh4gbag913@gmail.com>
2019-06-25 23:18:20 -0400

    Add kernel build script and flasher
    For creating flashable zips.
    
    Signed-off-by: shagbag913 <sh4gbag913@gmail.com>

---
+++ flasher/anykernel.sh
@@ -0,0 +1,32 @@
+# AnyKernel3 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() { '
+kernel.string=PR-OS kernel
+do.devicecheck=1
+do.modules=0
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=sailfish
+device.name2=marlin
+supported.versions=
+'; } # end properties
+
+# shell variables
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=1;
+ramdisk_compression=auto;
+
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. tools/ak3-core.sh;
+
+
+## AnyKernel install
+dump_boot;
+
+write_boot;
+
